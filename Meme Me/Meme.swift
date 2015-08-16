ba23cb83549d60cbaf3a65e31fef89c2df285d37
Lauren Bongartz <lbongartz@me.com>
2015-08-15 23:16:44 -0700

    Meme class
    Completed meme class.
    Need to resolve issue with saveMeme function.

--- Meme Me/Meme.swift
+++ Meme Me/Meme.swift
@@ -9,9 +9,39 @@ import Foundation
 import UIKit
 import CoreData
 
-objc(Meme)
+@objc(Meme)
 
-class Meme: NSManagedObject {
+class Meme: NSManagedObject, Equatable {
+    
+    struct Meme {
+        static let TopText = "top_text"
+        static let bottomText = "bottom_text"
+        static let originalImage = "original_image"
+        static let memedImage = "memed_image"
+    }
+    
+    @NSManaged var topText: String
+    @NSManaged var bottomText: String
+    @NSManaged var originalImage: UIImage
+    @NSManaged var memedImage: UIImage
+    
+    override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+    }
+    
+    init(dictionary: [String : AnyObject], context: NSManagedObjectContext) {
+        
+        // Core Data
+        let entity =  NSEntityDescription.entityForName("Meme", inManagedObjectContext: context)!
+        super.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        // Dictionary
+        topText = dictionary[Meme.TopText] as! String
+        bottomText = dictionary[Meme.bottomText] as! String
+        originalImage = dictionary[Meme.originalImage] as! UIImage
+        memedImage = dictionary[Meme.memedImage] as! UIImage
+    }
+}
 
 // Overload == function to utilize .find method
 func == (lhs: Meme, rhs: Meme) -> Bool {
@@ -22,21 +52,4 @@ func == (lhs: Meme, rhs: Meme) -> Bool {
             return true
     }
     return false
-}
-
-// Make Meme struct equatable to utilize .find method
-struct Meme: Equatable {
-    let topText: String
-    let bottomText: String
-    let originalImage: UIImage
-    let memedImage: UIImage
-
-    // Initiate struct.
-    init (topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage){
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-        self.memedImage = memedImage
-    }
-}
 }
